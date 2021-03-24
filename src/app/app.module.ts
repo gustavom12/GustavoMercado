82dca8731ad6b103019df1994fb7dc4f32fec9d5
julian mercado <gustavo.n.mercado2@gmail.com>
2021-03-23 21:53:57 -0300

    commit before update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {HttpClientModule} from "@angular/common/http";
-
 import { AboutComponent } from './components/about/about.component';
 import { HeaderComponent } from './components/header/header.component';
 import { navbarrComponent } from './components/navbar/navbar.component';
 import { PortfolioComponent } from './components/portfolio/portfolio.component';
 import { FormComponent } from './components/form/form.component';
 import { AppComponent } from './components/main-component/app.component';
-import { InfoComponent } from './components/portfolio/info/info.component';
 import { ConocimientosComponent } from './components/conocimientos/conocimientos.component';
 import { ProgresscircleComponent } from './components/progresscircle/progresscircle.component';
+import { LatestComponent } from './components/portfolio/latest/latest.component';
 
 @NgModule({
   declarations: [
@@ -20,9 +19,9 @@ import { ProgresscircleComponent } from './components/progresscircle/progresscir
     navbarrComponent,
     PortfolioComponent,
     FormComponent,
-    InfoComponent,
     ConocimientosComponent,
-    ProgresscircleComponent
+    ProgresscircleComponent,
+    LatestComponent
   ],
   imports: [
     BrowserModule,
